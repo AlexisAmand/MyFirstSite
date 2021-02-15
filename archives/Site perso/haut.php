0a2da7c108b1b1dce5858d7191ed010fc91310fc
alexisamand <alexis.amand@gmail.com>
2021-02-15 16:19:39 +0100

    first commit

---
+++ archives/Site perso/haut.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Document sans nom</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<style type="text/css">
+<!--
+.Style1 {color: #542}
+-->
+</style>
+</head>
+<body>
+<a href="http://www.phpmyvisites.net/" title="phpMyVisites | Open source web analytics"
+onclick="window.open(this.href);return(false);"><script type="text/javascript">
+<!--
+var a_vars = Array();
+var pagename='';
+
+var phpmyvisitesSite = 1;
+var phpmyvisitesURL = "http://tabouet.hostarea.org/phpmyvisites/phpmv2/phpmyvisites.php";
+//-->
+</script>
+
+
+
+<script language="javascript" src="http://tabouet.hostarea.org/phpmyvisites/phpmv2/phpmyvisites.js" type="text/javascript"></script>
+<object><noscript><p>phpMyVisites | Open source web analytics
+<img src="http://tabouet.hostarea.org/phpmyvisites/phpmv2/phpmyvisites.php" alt="Statistics" border="0">
+</p></noscript></object></a>
+<!-- /phpmyvisites --> 
+
+
+
+
+</body>
+</html>
---
+++ archives/Site perso/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Document sans nom</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+</head>
+
+<frameset rows="5%,*" frameborder="NO" border="0" framespacing="0">
+  <frame src="haut.php" name="topFrame" scrolling="NO" noresize >
+  <frame src="indexheredis.htm" name="mainFrame">
+</frameset>
+
+</html>
